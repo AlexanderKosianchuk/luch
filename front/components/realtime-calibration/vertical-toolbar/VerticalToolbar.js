e4bb21c80fdd3d348c79d52db3143c08378b8236
Alexander Kosianchuk <dedanaan00@gmail.com>
2017-12-22 17:58:13 +0200

    vertial toolbar complete

--- front/components/realtime-calibration/vertical-toolbar/VerticalToolbar.js
+++ front/components/realtime-calibration/vertical-toolbar/VerticalToolbar.js
@@ -10,14 +10,64 @@ import CalibrationSelector from 'controls/calibration-selector/CalibrationSelect
 
 import request from 'actions/request';
 
+const TOP_MENU_HEIGHT = 51;
+const MIN_WINDOW_WIDTH = 768; // check is mobile
+
 class VerticalToolbar extends Component {
+    static form = null;
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            sources: ['']
+        }
+    }
+
+    componentDidMount() {
+        if (window.innerWidth > MIN_WINDOW_WIDTH) {
+            this.form.style.height = (window.innerHeight - TOP_MENU_HEIGHT) + 'px';
+        }
+    }
+
+    buildIpsInputs() {
+        let ips = [];
+        console.log(this.state.sources);
+        for (let ii = 0; ii < this.state.sources.length; ii++) {
+            ips.push(<input
+                key={ ii }
+                className='form-control'
+                name='ip[]'
+                value={ this.state.sources[ii] }
+                onChange={ this.handleChange.bind(this, ii) }
+                />
+            );
+        }
+
+        return ips;
+    }
+
+    handleChange(index, event) {
+        let sources = this.state.sources.slice();
+        sources[index] = event.target.value;
+
+        this.setState({ sources: sources });
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+
+        let sources = this.state.sources.slice();
+        sources.push('');
+
+        this.setState({ sources: sources });
     }
 
     render() {
         return (
-            <div className='realtime-calibration-vertical-toolbar'
+            <form
+                className='realtime-calibration-vertical-toolbar'
+                ref={ (form) => { this.form = form; } }
             >
                 <div className='realtime-calibration-vertical-toolbar__connection-params'>
                     <Translate value='realtimeCalibration.verticalToolbar.connectionParams'/>
@@ -31,7 +81,29 @@ class VerticalToolbar extends Component {
                         <CalibrationSelector/>
                     </ul>
                 </div>
-            </div>
+                <div className='realtime-calibration-vertical-toolbar__label'>
+                    <Translate value='realtimeCalibration.verticalToolbar.fdrType'/>
+                </div>
+                <div className='realtime-calibration-vertical-toolbar__controll'>
+                    <select className='form-control' name='connectionType'>
+                        <option value='udp'>UDP</option>
+                    </select>
+                </div>
+                <div className='realtime-calibration-vertical-toolbar__label'>
+                    <Translate value='realtimeCalibration.verticalToolbar.fdrType'/>
+                </div>
+                <div className='realtime-calibration-vertical-toolbar__controll'>
+                    { this.buildIpsInputs() }
+                </div>
+                <div className='realtime-calibration-vertical-toolbar__button'>
+                    <button
+                        className='btn btn-default'
+                        onClick={ this.handleClick.bind(this) }
+                    >
+                        <Translate value='realtimeCalibration.verticalToolbar.fdrType'/>
+                    </button>
+                </div>
+            </form>
         );
     }
 }
