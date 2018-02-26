e852be613d462abb0f5b6de50b59250b6184620c
AlexanderKosianchuk <dedanaan00@gmail.com>
2018-02-26 13:52:24 +0200

    phinx db example add

---
+++ back/config/example.db.phinx.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'phinx' => [
+    'adapter' => 'mysql',
+    'host' => 'localhost',
+    'name' => 'luche_common',
+    'user' => 'commonUser',
+    'pass' => '4cjCnDs2YtSwGlPL',
+    'port' => '3306',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci'
+  ]
+];
--- back/config/example.db.redis.php
+++ back/config/example.db.redis.php
@@ -3,8 +3,8 @@
 return [
   'default' => [
     'scheme' => 'tcp',
-    'host'   => '127.0.0.1',
-    'port'   => 6379,
+    'host' => '127.0.0.1',
+    'port' => 6379,
     'password' => ''
-  ]:
+  ]
 ];
